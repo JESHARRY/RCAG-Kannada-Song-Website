52d7bbfc951b489b6afdd86de6a77317cf6c9eee
Harish P C <harishpcstk2224@gmail.com>
2026-09-01 07:02:03 +0530

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/RCAG-Kannada-Song-Website/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
